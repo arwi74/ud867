9687695eddfbb3416cd0dcd51e997beab44dc80c
Arkadiusz Wilczek <arekwil@gmail.com>
2018-05-14 16:00:12 +0200

    added android library

---
+++ FinalProject/javaJokes/src/main/java/com/example/javajokes/Joker.java
@@ -0,0 +1,8 @@
+package com.example.javajokes;
+
+public class Joker {
+
+    public String getJoke() {
+        return "Nuck, nuck ";
+    }
+}
---
+++ FinalProject/jokedisplay/src/main/java/com/example/jokedisplay/jokeActivityFragment.java
@@ -0,0 +1,31 @@
+package com.example.jokedisplay;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class jokeActivityFragment extends Fragment {
+
+    public jokeActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_joke, container, false);
+        TextView jokeTextView = view.findViewById(R.id.fragment_joke_text_view);
+        Intent intent = getActivity().getIntent();
+        String joke = intent.getStringExtra(jokeActivity.EXTRA_JOKE);
+        if ( joke != null ) {
+            jokeTextView.setText(joke);
+        }
+        return view;
+    }
+}
